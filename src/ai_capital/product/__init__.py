70b1236a31c3b5012377bc6819faa9c8cd9787cd
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-09-12 09:54:33 +0300

    H2.5: expose workspace product operator

--- src/ai_capital/product/__init__.py
+++ src/ai_capital/product/__init__.py
@@ -3,9 +3,11 @@
 from .actor_provider import LocalActorProviderOperator
 from .program_operator import LocalProgramOperator
 from .provider_operator import LocalProviderOperator
+from .workspace_operator import LocalWorkspaceOperator
 
 __all__ = [
     "LocalActorProviderOperator",
     "LocalProgramOperator",
     "LocalProviderOperator",
+    "LocalWorkspaceOperator",
 ]
